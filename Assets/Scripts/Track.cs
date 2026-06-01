eb365fab1518b1e8ffbea0be44fb6cb458d5ab27
Martin McCarthy <mccarthy.martinc@gmail.com>
2026-06-01 14:38:14 -0400

    update for drawing ties around a spline

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -1,124 +1,240 @@
 using System.Collections.Generic;
-using System.Linq;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Splines;
+using UnityEngine.UIElements;
 
 public class Track : MonoBehaviour
 {
-    #region Mesh Settings
-    
-    public float Resolution = 1.0f; // mesh fidelity, higher = smoother curves at cost of more polys
-    
+    Spline spline;
 
-    #endregion
-    
-    MiniMesh railMesh;
+    // mesh generation
+    public int samples = 6;
+    public int railSides = 8;
+    public float trackWidth = 1.435f;
+    public float railRadius = 0.05f;
+    public float tieSpace = 0.5f;
+    public float tieHeight = 0.2f;
+    public float tieWidth = 0.6f;
+    public float tieDepth = 0.12f;
 
-    struct Vertex
+    private void Start()
     {
-        public Vector3 pos, norm;
-        public Vector2 uv;
+        spline = GetComponent<SplineContainer>().Splines[0];
+
+        SampleSpline(out Vector3[] positions, out Vector3[] tangents, out Vector3[] ups, out Vector3[] rights);
+        BuildMesh(positions, tangents, ups, rights);
     }
 
-    // only need a few things from the mesh class (verts tris)
-    class MiniMesh
+    private void SampleSpline(out Vector3[] positions, out Vector3[] tangents, out Vector3[] ups, out Vector3[] rights)
     {
-        public List<Vertex> verts = new();
-        public List<int> tris = new();
-    }
+        float length = spline.GetLength();
 
-    Mesh generatedMesh;
-    bool isDrawn = false;
+        // in regards to total samples here the reason for 4 is to make sure the mesh doesn't break, but it's arbitrary
+        int totalSamples = Mathf.Max(4, Mathf.RoundToInt(length * samples));
 
-    void Start()
-    {
-        if(!isDrawn)
+        Debug.Log($"Length: {length}, Total Samples: {totalSamples}");
+
+        positions = new Vector3[totalSamples];
+        tangents = new Vector3[totalSamples];
+        ups = new Vector3[totalSamples];
+        rights = new Vector3[totalSamples];
+
+        for (int i = 0; i < totalSamples; i++)
         {
-            DrawMesh();
+            float t = i / (totalSamples - 1f);
+            spline.Evaluate(t, out float3 position, out float3 tangent, out float3 up);
+            positions[i] = (Vector3)position;
+            tangents[i] = (Vector3)tangent;
+            ups[i] = (Vector3)up;
+            rights[i] = Vector3.Cross((Vector3)up, (Vector3)tangent).normalized;
+        }
+
+        Debug.Log($"{positions}, {tangents}, {ups}, {rights}");
+    }
+
+    private void BuildMesh(Vector3[] positions, Vector3[] tangents, Vector3[] ups, Vector3[] rights)
+    {
+        List<Vector3> verts = new();
+        List<int> tris = new();
+        List<Vector2> uvs = new();
+
+        float halfWidth = trackWidth * 0.5f;
+        BuildRail(verts, tris, uvs, positions, rights, ups, halfWidth);
+        BuildRail(verts, tris, uvs, positions, rights, ups, -halfWidth);
+
+        Debug.Log($"Verts: {verts.Count}, Tris: {tris.Count / 3}, UVs: {uvs.Count}");
+
+        BuildTies(verts, tris, uvs, positions, rights, ups);
+        DrawMesh(verts, tris.ToArray(), uvs);
+    }
+
+    private void BuildRail(List<Vector3> verts, List<int> tris, List<Vector2> uvs, Vector3[] positions, Vector3[] rights, Vector3[] ups, float halfWidth)
+    {
+        int sides = Mathf.Max(3, railSides);
+        int samples = positions.Length;
+
+        // When you call BuildRail() twice (once for each rail), the second call is appending vertices into the same verts list that already has the first rail's vertices in it.
+        int baseIdx = verts.Count;
+
+        float uStep = 1f / sides;
+
+        for (int i = 0; i < samples; i++)
+        {
+            Vector3 c = positions[i] + rights[i] * halfWidth;
+
+            float t = i / (samples - 1f);
+            for (int side = 0; side < sides; side++)
+            {
+                float theta = side / sides * Mathf.PI * 2f;
+
+                Vector3 offset = (rights[i] * Mathf.Cos(theta) + ups[i] * Mathf.Sin(theta)) * railRadius;
+
+                verts.Add(c + offset);
+                uvs.Add(new Vector2(side * uStep, t));
+            }
+        }
+
+        for (int i = 0; i < samples - 1; i++)
+        {
+            for (int side = 0; side < sides; side++)
+            {
+                int nextSide = (side + 1) % sides;
+
+                int a = baseIdx + i * sides + side;
+                int b = baseIdx + i * sides + nextSide;
+                int c = baseIdx + (i + 1) * sides + side;
+                int d = baseIdx + (i + 1) * sides + nextSide;
+
+                tris.Add(a);
+                tris.Add(c);
+                tris.Add(b);
+
+                tris.Add(b);
+                tris.Add(c);
+                tris.Add(d);
+            }
+        }
+
+        if (!spline.Closed)
+            AddDiskCap();
+    }
+
+    // do this later im lazy af
+    private void AddDiskCap()
+    {
+
+    }
+
+
+    private void BuildTies(List<Vector3> verts, List<int> tris, List<Vector2> uvs, Vector3[] positions, Vector3[] rights, Vector3[] ups)
+    {
+        float length = spline.GetLength();
+        int totalSamples = Mathf.Max(4, Mathf.RoundToInt(length * samples));
+
+        float interval = tieSpace * 0.5f;
+        float inverse = 1f / length;
+
+        while (interval < length)
+        {
+            float t = Mathf.Clamp01(interval * inverse);
+
+            spline.Evaluate(t, out float3 position, out float3 tangent, out float3 up);
+            Vector3 fwd = math.normalizesafe(tangent);
+            Vector3 upN = math.normalizesafe(up);
+            Vector3 right = Vector3.Cross(upN, fwd).normalized;
+
+            Vector3 c = (Vector3)position - upN * (railRadius + tieHeight * 0.5f);
+            AddBox(verts, tris, uvs, c, right, upN, fwd, tieWidth, tieHeight, tieDepth);
+
+            interval += tieSpace;
         }
     }
 
-    void DrawMesh()
+    private void AddBox(List<Vector3> verts, List<int> tris, List<Vector2> uvs, Vector3 center, Vector3 right, Vector3 up, Vector3 forward, float width, float height, float depth)
     {
-        generatedMesh = new Mesh();
-        generatedMesh.name = "Rail";
+        float hx = width * 0.5f;
+        float hy = height * 0.5f;
+        float hz = depth * 0.5f;
 
-    }
-
-    // TODO: look into what "caps" are in the ZFTrack, not exactly sure what they do
-    void SetMeshes(Mesh sourceRail, Mesh sourceTie)
-    {
-        railMesh = new();
-        MiniMesh tieMesh = new();
-
-        Vector3[] verts = sourceRail.vertices;
-        Vector3[] norms = sourceRail.normals;
-        Vector2[] uvs = sourceRail.uv;
-        int[] tris = sourceRail.triangles;
-        Dictionary<int, int> railTris = new();
-
-        // maps an original mesh vertex index to the destination mesh, copying vertex on first use
-        System.Func<int, Dictionary<int, int>, MiniMesh, int> mapVert = (v, mapping, dest) =>
+        Vector3[] corners = new Vector3[8]
         {
-            int pos;
-            if (mapping.TryGetValue(v, out pos)) return pos; // see if we've already copied this vert
-            else // otherwise create a new vert
-            {
-                dest.verts.Add(new Vertex
-                {
-                    pos = verts[v],
-                    norm = norms[v],
-                    uv = uvs[v]
-                });
-                int newV = dest.verts.Count - 1;
-                mapping[v] = newV;
-                return newV;
-            }
+            center + (-right * hx) + (-up * hy) + (-forward * hz),
+            center + (right * hx) + (-up * hy) + (-forward * hz),
+            center + (right * hx) + (up * hy) + (-forward * hz),
+            center + (-right * hx) + (up * hy) + (-forward * hz),
+            center + (-right * hx) + (-up * hy) + (forward * hz),
+            center + (right * hx) + (-up * hy) + (forward * hz),
+            center + (right * hx) + (up * hy) + (forward * hz),
+            center + (-right * hx) + (up * hy) + (forward * hz)
         };
 
-        void AddFace(int v1, int v2, int v3, Dictionary<int, int> mapping, MiniMesh dest)
+        int[,] faces = new int[6, 4]
         {
-            v1 = mapVert(v1, mapping, dest);
-            v2 = mapVert(v2, mapping, dest);
-            v3 = mapVert(v3, mapping, dest);
+            { 0, 1, 2, 3 }, // back   (-forward)
+            { 5, 4, 7, 6 }, // front  (+forward)
+            { 4, 0, 3, 7 }, // left   (-right)
+            { 1, 5, 6, 2 }, // right  (+right)
+            { 4, 5, 1, 0 }, // bottom (-up)
+            { 3, 2, 6, 7 }, // top    (+up)
+        };
 
-            dest.tris.Add(v1);
-            dest.tris.Add(v2);
-            dest.tris.Add(v3);
-        }
+        Vector2[] faceUVs = {
+            new (0, 0),
+            new (1, 0),
+            new (1, 1),
+            new (0, 1)
+        };
 
-        for(int i = 0; i < tris.Length; i += 3)
+        for (int i = 0; i < 6; i++)
         {
-            int v1 = tris[i], v2 = tris[i + 1], v3 = tris[i + 2];
-            int sideCount = (verts[v1].z > 0 ? 0 : 1) + (verts[v2].z > 0 ? 0 : 1) + (verts[v3].z > 0 ? 0 : 1);
-            if(sideCount == 0 || sideCount == 3)
+            int baseIdx = verts.Count;
+            for (int j = 0; j < 4; j++)
             {
+                verts.Add(corners[faces[i, j]]);
+                uvs.Add(faceUVs[j]);
             }
-            else
-            {
-                AddFace(v1, v2, v3, railTris, railMesh);
-            }
+            tris.Add(baseIdx);
+            tris.Add(baseIdx + 2);
+            tris.Add(baseIdx + 1);
+
+            tris.Add(baseIdx);
+            tris.Add(baseIdx + 3);
+            tris.Add(baseIdx + 2);
         }
     }
 
-    void GenerateMesh(Mesh mesh)
+    private void DrawMesh(List<Vector3> verts, int[] tris, List<Vector2> uvs)
     {
-        if(railMesh == null)
-        {
-            mesh.Clear();
-            return;
-        }
+        Mesh mesh = new();
+        mesh.name = "TrackMesh";
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        mesh.SetVertices(verts);
+        mesh.SetTriangles(tris, 0);
+        mesh.SetUVs(0, uvs);
 
-        List<Vector3> allVerts = new();
-        List<Vector3> allNorms = new();
-        List<Vector2> allUVs = new();
-        List<int> allTris = new();
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
 
-        int vertOffset = 0;
-        // SimpleTransform
+        GetComponent<MeshFilter>().sharedMesh = mesh;
     }
 
-    //float[] GetSteps()
-    //{
-    //    // int steps = (int)Mathf.Clamp(Mathf.Round())
-    //}
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        var spline = GetComponent<SplineContainer>().Spline;
+        int steps = 64;
+        Vector3 prev = transform.TransformPoint((Vector3)spline.EvaluatePosition(0f));
+        Gizmos.color = Color.yellow;
+        for (int i = 1; i <= steps; i++)
+        {
+            Vector3 next = transform.TransformPoint(
+                (Vector3)spline.EvaluatePosition((float)i / steps));
+            Gizmos.DrawLine(prev, next);
+            prev = next;
+        }
+    }
+#endif
+
 }
